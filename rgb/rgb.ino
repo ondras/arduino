d26e018fbb9a52ac7a06eb92d86b74c144244522
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2014-06-04 10:41:36 +0200

    rgb

---
+++ rgb/rgb.ino
@@ -0,0 +1,29 @@
+#define N 3
+#define MAX 256
+
+byte pins[N] = {11, 10, 9};
+byte rgb[N] = {0, 0, 0};
+
+byte index = 0;
+int direction = 1;
+
+void setup() {
+	Serial.begin(9600);
+	for (byte i=0; i<N; i++) { pinMode(pins[i], OUTPUT); }
+}
+
+void write() {
+	for (byte i=0; i<N; i++) { analogWrite(pins[i], rgb[i]); }
+}
+
+void loop() {
+	index = (index + 1 + random(N-1)) % N;
+	direction = (rgb[index] ? -1 : 1);
+
+	for (int i=1; i<MAX; i++) {
+		rgb[index] += direction;
+
+		write();
+		delay(5);
+	}
+}
