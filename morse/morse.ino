62424d6a25daf1fdef31d2f24013b7e437c1fa76
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2014-07-07 11:01:31 +0200

    interruptible morse

--- morse/morse.ino
+++ morse/morse.ino
@@ -5,9 +5,18 @@
 #define N 8
 #define SPEED 1
 #define DELAY 200*SPEED
+#define BLINK(length) led_set(true); delay(length*DELAY); led_set(false); delay(DELAY)
+#define LIMIT 150
 
-LedControl lc(12, 11, 3, 1); 
+/*
+ * Pin 12 is connected to the DATA IN-pin of the first MAX7221
+ * Pin 11 is connected to the CLK-pin of the first MAX7221
+ * Pin 10 is connected to the LOAD(/CS)-pin of the first MAX7221 	
+ * There will only be a single MAX7221 attached to the arduino 
+ */
+LedControl lc(12, 11, 10, 1); 
 String code;
+int index = 0;
 
 void led_set(bool value) {
 	for (byte i=0;i<N;i++) {
@@ -17,31 +26,22 @@ void led_set(bool value) {
 void setup() {
 	lc.setIntensity(0, 0);
 	lc.shutdown(0, false);
-	code = morse("juch");
+	code = morse("tajny kod");
 }
 
 void loop() {
-	int len = code.length();
-	char ch;
-	for (int i=0;i<len;i++) {
-		ch = code[i];
-		switch (ch) {
-			case '.':
-				led_set(true);
-				delay(DELAY);
-				led_set(false);
-				delay(DELAY);
-			break;
-			case '-':
-				led_set(true);
-				delay(3*DELAY);
-				led_set(false);
-				delay(DELAY);
-			break;
-			case '/':
-				delay(4*DELAY);
-			break;
-		}
+	if (analogRead(A0) > LIMIT) {
+		index = 0;
+		return;
 	}
-}
 
+	char ch = code[index];
+	switch (ch) {
+		case '.': BLINK(1); break;
+		case '-': BLINK(3); break;
+		case '/': delay(4*DELAY); break;
+	}
+
+	index++;
+	if (index >= code.length()) { index = 0; }
+}
