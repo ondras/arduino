fb7e63b868c56ca4c2031a27f01d90ed77b92063
Ondřej Žára <ondrej.zara@gmail.com>
2020-07-18 23:47:41 +0200

    bme cleanup

--- bme280/bme280.ino
+++ bme280/bme280.ino
@@ -1,21 +1,4 @@
-/***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BMEP280 Breakout
-  ----> http://www.adafruit.com/products/2651
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
-#include "secrets.h"
+#include "config.h"
 
 #include <Wire.h>
 #include <SPI.h>
@@ -33,53 +16,49 @@
 #define MQTT_DEBUG
 #define MQTT_ERROR
 
-
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 
 Adafruit_BME280 bme; // I2C
 WiFiClient client;
 
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-
-Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/kancelar.temperature");
-Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/kancelar.humidity");
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, AIO_FEED_TEMPERATURE);
+Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, AIO_FEED_HUMIDITY);
 
 
 // connect to adafruit io via MQTT
 void connect() {
-  Serial.print(F("Connecting to Adafruit IO... "));
+  Serial.print(F("WiFi: Connecting to "));
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+  Serial.println(F("WiFi: connected"));
+  Serial.println(F("WiFi IP address: "));
+  Serial.println(WiFi.localIP());
+
+  Serial.print(F("MQTT: Connecting to Adafruit IO... "));
   int8_t ret;
-
   while ((ret = mqtt.connect()) != 0) {
     Serial.println(mqtt.connectErrorString(ret));
-    /*
-    switch (ret) {
-      case 1: Serial.println(F("Wrong protocol")); break;
-      case 2: Serial.println(F("ID rejected")); break;
-      case 3: Serial.println(F("Server unavail")); break;
-      case 4: Serial.println(F("Bad user/pass")); break;
-      case 5: Serial.println(F("Not authed")); break;
-      case 6: Serial.println(F("Failed to subscribe")); break;
-      default: Serial.println(F("Connection failed")); Serial.println(ret); break;
-    }
-*/
-    if (ret >= 0)
-      mqtt.disconnect();
-
-    Serial.println(F("Retrying connection..."));
+    if (ret >= 0) mqtt.disconnect();
+    Serial.println();
+    Serial.println(F("MQTT: Retrying connection..."));
     delay(5000);
-
   }
 
-  Serial.println(F("Adafruit IO Connected!"));
+  Serial.println();
+  Serial.println(F("MQTT: Adafruit IO Connected!"));
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("BMP280 test"));
+  Serial.println(F("Initializing"));
 
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
@@ -93,29 +72,13 @@ void setup() {
                   Adafruit_BME280::FILTER_OFF   );
 
 
-  // Connect to WiFi access point.
-  delay(10);
-  Serial.print(F("Connecting to "));
-  Serial.println(WLAN_SSID);
-
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(F("."));
-  }
-  Serial.println();
-  Serial.println(F("WiFi connected"));
-  Serial.println(F("IP address: "));
-  Serial.println(WiFi.localIP());
-
+  delay(100);
   connect();
 }
 
 void loop() {
-  if (! mqtt.ping(3)) {
-    // reconnect to adafruit io
-    if (! mqtt.connected())
-      connect();
+  if (!mqtt.ping(3)) {
+    if (!mqtt.connected()) { connect(); }
   }
 
   bme.takeForcedMeasurement();
@@ -143,5 +106,5 @@ void loop() {
 
       Serial.println();
   */
-  delay(30000);
+  delay(DELAY);
 }
